ae84cc6f0806cf5dff28fae5ffa3c7306254cfbd
Andy Ryan <john.ryan@nenaner.com>
2025-02-05 20:59:59 -0500

    https://docs.expo.dev/tutorial/build-a-screen/

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Image} from 'expo-image';
+import { StyleSheet, View } from "react-native";
 
+import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
@@ -13,6 +13,10 @@ export default function Index() {
       <View style={styles.imageContainer}>
           <ImageViewer imgSource={PlaceholderImage} />
       </View>
+        <View style={styles.footerContainer}>
+            <Button label="Choose a photo"/>
+            <Button label="Use this photo"/>
+        </View>
     </View>
   );
 }
@@ -25,5 +29,9 @@ const styles = StyleSheet.create({
     },
     imageContainer: {
         flex: 1,
-    }
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
 });
